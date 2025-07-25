40b853f48b76f600c97f0d45b252e8cc83025a8c
Kaito Udagawa <umireon@gmail.com>
2025-07-26 02:32:46 +0900

    Fix

--- packages/strapi/image/config/server.ts
+++ packages/strapi/image/config/server.ts
@@ -1,4 +1,4 @@
-module.exports = ({ env }) => ({
+export default ({ env }) => ({
   host: env("HOST", "::"),
   port: env.int("PORT", 1337),
   app: {
---
+++ packages/strapi/image/src/admin/app.tsx
@@ -0,0 +1,10 @@
+import type { StrapiApp } from "@strapi/strapi/admin";
+
+export default {
+  config: {
+    locales: ["ja"],
+  },
+  bootstrap(app: StrapiApp) {
+    console.log(app);
+  },
+};
