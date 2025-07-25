9240adf046b2f1f04493175fb0373644e91ce996
Kaito Udagawa <umireon@gmail.com>
2025-07-26 06:33:12 +0900

    Fix

---
+++ packages/web/src/interfaces/Novel.ts
@@ -0,0 +1,13 @@
+export default interface Noval {
+  id: number;
+  Body: string;
+  Date: string;
+  Description: string;
+  Title: string;
+
+  slug: string;
+
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+}
