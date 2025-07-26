8943f1c07250a14370ac0d5a552d0a30651e42d6
Kaito Udagawa <umireon@gmail.com>
2025-07-27 06:22:35 +0900

    Fix

--- packages/strapi/image/src/api/novel/content-types/novel/lifecycles.ts
+++ packages/strapi/image/src/api/novel/content-types/novel/lifecycles.ts
@@ -1,19 +1,19 @@
 export default {
     async afterCreate(event) {
         const { result } = event;
-        strapi.plugin('plugin::my-github-actions-dispatcher.dispatch')
+        strapi.service('plugin::my-github-actions-dispatcher.dispatch')
         .triggerDispatch('strapi-novel-create', { data: result });
     },
 
     async afterUpdate(event) {
         const { result } = event;
-        strapi.plugin('plugin::my-github-actions-dispatcher.dispatch')
+        strapi.service('plugin::my-github-actions-dispatcher.dispatch')
         .triggerDispatch('strapi-novel-update', { data: result });
     },
 
     async afterDelete(event) {
         const { result } = event;
-        strapi.plugin('plugin::my-github-actions-dispatcher.dispatch')
+        strapi.service('plugin::my-github-actions-dispatcher.dispatch')
         .triggerDispatch('strapi-novel-delete', { data: result });
     }
 };
--- packages/strapi/image/src/plugins/my-github-actions-dispatcher/server/src/services/index.ts
+++ packages/strapi/image/src/plugins/my-github-actions-dispatcher/server/src/services/index.ts
@@ -1,5 +1,7 @@
+import dispatch from './dispatch';
 import service from './service';
 
 export default {
   service,
+  dispatch,
 };
