56b721e9ed6155309197e776916eb21a42ee9601
Kaito Udagawa <umireon@kaito.tokyo>
2025-08-13 10:09:23 +0900

    Update index.ts (#47)

--- packages/web/worker/index.ts
+++ packages/web/worker/index.ts
@@ -4,18 +4,12 @@ export default {
     env: Env,
     ctx: ExecutionContext,
   ): Promise<Response> {
-    const url = new URL(request.url);
-
-    if (url.pathname.startsWith("/vip")) {
-      const authorizationHeader = request.headers.get("Authorization");
-
-      if (!authorizationHeader) {
-        return new Response("Unauthorized", { status: 401 });
-      }
-    }
-
-    // If not a /vip path or Authorization header is present, proceed with the request
     const response = await env.ASSETS.fetch(request);
+
+    const url = new URL(request.url);
+    if (!url.pathname.startsWith("/vip")) {
+      return response;
+    }
 
     const cfAccessJwtAssertion = request.headers.get("Cf-Access-Jwt-Assertion");
 
