c9b264308fa9441fd965ba0a175ca5690f2db1b3
Kaito Udagawa <umireon@kaito.tokyo>
2025-08-14 20:37:30 +0900

    Add required Date field to roleplay-actor schema (#55)
    Introduces a new required 'Date' field of type 'date' to the roleplay-actor content type schema and updates the corresponding TypeScript definitions to reflect this change.

--- packages/strapi/image/types/generated/contentTypes.d.ts
+++ packages/strapi/image/types/generated/contentTypes.d.ts
@@ -517,6 +517,7 @@ export interface ApiRoleplayActorRoleplayActor
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<"oneToOne", "admin::user"> &
       Schema.Attribute.Private;
+    Date: Schema.Attribute.Date & Schema.Attribute.Required;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       "oneToMany",
