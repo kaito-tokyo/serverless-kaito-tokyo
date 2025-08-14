0386e3593034d3a5a6c3fc44a7218a1d431d4501
Kaito Udagawa <umireon@kaito.tokyo>
2025-08-14 20:01:56 +0900

    Update rotate_secrets.sh (#54)

--- packages/strapi/rotate_secrets.sh
+++ packages/strapi/rotate_secrets.sh
@@ -2,13 +2,13 @@
 
 r()
 {
-  openssl rand -base64 64 | tr -d '\n'
+  openssl rand -base64 "$1" | tr -d '\n'
 }
 
-echo "$(r),$(r),$(r),$(r)" | npx wrangler secret put APP_KEYS
+echo "$(r 64),$(r 64),$(r 64),$(r 64)" | npx wrangler secret put APP_KEYS
 
-r | npx wrangler secret put ADMIN_JWT_SECRET
-r | npx wrangler secret put API_TOKEN_SALT
-r | npx wrangler secret put ENCRYPTION_KEY
-r | npx wrangler secret put JWT_SECRET
-r | npx wrangler secret put TRANSFER_TOKEN_SALT
+r 64 | npx wrangler secret put ADMIN_JWT_SECRET
+r 64 | npx wrangler secret put API_TOKEN_SALT
+r 64 | npx wrangler secret put ENCRYPTION_KEY
+r 64 | npx wrangler secret put JWT_SECRET
+r 64 | npx wrangler secret put TRANSFER_TOKEN_SALT
